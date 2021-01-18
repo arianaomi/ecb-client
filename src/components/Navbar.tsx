f786f2db50cf4c01b0a5c5f7c29f690223480f7e
Arianaomi <arianaomi.lp@gmail.com>
2021-01-18 09:12:58 -0600

    add alt property

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ import logo from '../ecb.png'
 export default function Navbar () {
   return (
     <div className='navbar'>
-      <img src={logo} className='logo' />
+      <img src={logo} className='logo' alt='log'/>
       <div className='wrapper-items'>
         <Link to='/' className='link'>
           <h2 className='item'>Carros en servicio</h2>
